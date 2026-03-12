7062cac5d7fb968b734aae6d8bc03ebfc5911648
polina <karamyshevapolina@yandex.ru>
2026-03-12 22:20:35 +0300

    Initial Commit

---
+++ travel-guide-new/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  travel-guide-new
+//
+//  Created by Полина on 12.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
