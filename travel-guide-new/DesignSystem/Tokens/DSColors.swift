1474db3320bff86a128ab080559bd0cdfa517035
polina <karamyshevapolina@yandex.ru>
2026-04-09 19:38:49 +0300

    fix

---
+++ travel-guide-new/DesignSystem/Tokens/DSColors.swift
@@ -0,0 +1,103 @@
+import UIKit
+
+extension DS {
+    enum Color {
+        static let background = UIColor(
+            red: 248.0 / 255.0,
+            green: 244.0 / 255.0,
+            blue: 236.0 / 255.0,
+            alpha: 1
+        )
+        static let surface = UIColor(
+            red: 255.0 / 255.0,
+            green: 253.0 / 255.0,
+            blue: 248.0 / 255.0,
+            alpha: 1
+        )
+        static let surfaceRaised = UIColor(
+            red: 222.0 / 255.0,
+            green: 226.0 / 255.0,
+            blue: 242.0 / 255.0,
+            alpha: 0.55
+        )
+
+        static let text = UIColor(
+            red: 24.0 / 255.0,
+            green: 23.0 / 255.0,
+            blue: 21.0 / 255.0,
+            alpha: 1
+        )
+        static let brand = UIColor(
+            red: 17.0 / 255.0,
+            green: 95.0 / 255.0,
+            blue: 101.0 / 255.0,
+            alpha: 1
+        )
+        static let textSubtle = UIColor(
+            red: 92.0 / 255.0,
+            green: 88.0 / 255.0,
+            blue: 83.0 / 255.0,
+            alpha: 1
+        )
+        static let textInverse = UIColor(
+            red: 252.0 / 255.0,
+            green: 251.0 / 255.0,
+            blue: 248.0 / 255.0,
+            alpha: 1
+        )
+        static let brandPressed = UIColor(
+            red: 13.0 / 255.0,
+            green: 78.0 / 255.0,
+            blue: 84.0 / 255.0,
+            alpha: 1
+        )
+
+        static let danger = UIColor(
+            red: 210.0 / 255.0,
+            green: 85.0 / 255.0,
+            blue: 78.0 / 255.0,
+            alpha: 1
+        )
+        static let dangerPressed = UIColor(
+            red: 188.0 / 255.0,
+            green: 70.0 / 255.0,
+            blue: 63.0 / 255.0,
+            alpha: 1
+        )
+
+        static let success = UIColor(
+            red: 74.0 / 255.0,
+            green: 156.0 / 255.0,
+            blue: 94.0 / 255.0,
+            alpha: 1
+        )
+
+        static let border = UIColor(
+            red: 217.0 / 255.0,
+            green: 211.0 / 255.0,
+            blue: 200.0 / 255.0,
+            alpha: 1
+        )
+        static let borderFocused = UIColor(
+            red: 79.0 / 255.0,
+            green: 140.0 / 255.0,
+            blue: 147.0 / 255.0,
+            alpha: 1
+        )
+
+        static let disabledBackground = UIColor(
+            red: 236.0 / 255.0,
+            green: 231.0 / 255.0,
+            blue: 222.0 / 255.0,
+            alpha: 1
+        )
+        static let disabledText = UIColor(
+            red: 163.0 / 255.0,
+            green: 157.0 / 255.0,
+            blue: 146.0 / 255.0,
+            alpha: 1
+        )
+
+        static let transparent = UIColor.clear
+    }
+}
---
+++ travel-guide-new/DesignSystem/Tokens/DSSpacing.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+extension DS {
+    enum Space {
+        static let space025: CGFloat = 2
+        static let space050: CGFloat = 4
+        static let space100: CGFloat = 8
+        static let space150: CGFloat = 12
+        static let space200: CGFloat = 16
+        static let space300: CGFloat = 24
+        static let space400: CGFloat = 32
+    }
+}
